db9e20c7d892ff72c5739105e972f373c36c25a5
yuvalg99 <yuvalyoav7@gmail.com>
2020-11-21 14:22:41 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pymergecap',
+    version='0.0.1',
+    packages=['pymergecap'],
+    url='https://github.com/yuvalg99/pymergecap',
+    license='',
+    author='Yuval Goihberg',
+    author_email='yuvalyoav7@gmail.com',
+    description='mergecap wrapper written in python.'
+)
